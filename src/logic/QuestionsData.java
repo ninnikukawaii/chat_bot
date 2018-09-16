594916a652be8fdb47114f1e4790cf1935e8ecc6
TheDoctorTimeLord <38783627+TheDoctorTimeLord@users.noreply.github.com>
2018-09-17 01:36:26 +0500

    Revert "Merge branch 'master' of https://github.com/TheDoctorTimeLord/chat_bot"
    This reverts commit 06a8e024f1f290ff84c5626b9613ee90faafc6a7, reversing
    changes made to eb1645d267ae2fcf035f5c849c115ad3efedcf69.

--- src/logic/QuestionsData.java
+++ src/logic/QuestionsData.java
@@ -1,35 +1,11 @@
 package logic;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Random;
-
 public class QuestionsData {
-    private ArrayList<Question> mQuestions;
-    private Random random;
-
     public QuestionsData(String filename) {
-        mQuestions = new ArrayList<Question>();
-        mQuestions.add(new Question("lol?", "kek"));
-
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(filename))){
-            String line;
-            while ((line = bufferedReader.readLine()) != null) {
-                String[] lineParts = line.split("|");
-                mQuestions.add(new Question(lineParts[0], lineParts[1]));
-            }
-        }
-        catch (IOException exception){
-            System.out.println(exception.getMessage());
-        }
-
-        random = new Random();
+        //считать базу вопросов и сформировать сами вопросы
     }
 
     public Question getQuestion() {
-        int index = random.nextInt(mQuestions.size());
-        return mQuestions.get(index);
+        //возвращаем случайный вопрос из имеющихся
     }
 }
