7e03a9303d523d6b2f50e93818ad7b275127ab4a
TheDoctorTimeLord <38783627+TheDoctorTimeLord@users.noreply.github.com>
2018-09-17 00:04:00 +0500

    структура вопроса

--- src/logic/Question.java
+++ src/logic/Question.java
@@ -1,12 +1,28 @@
 package logic;
 
 public class Question {
-    private String question;
-    private String answer;
+    private String mQuestion;
+    private String mAnswer;
 
     public Question(String question, String answer){
-        // инициализицация вопроса
+        mQuestion = question;
+        mAnswer = answer;
     }
 
-    //иметь геттеры обеих полей
+    public String getQuestion(){
+        return mQuestion;
+    }
+
+    public String getAnswer() {
+        return mAnswer;
+    }
+
+    @Override
+    public boolean equals(Object object){
+        if (object instanceof Question){
+            Question question = (Question) object;
+            return question.mQuestion.equals(mQuestion) && question.mAnswer.equals(mAnswer);
+        }
+        return false;
+    }
 }
