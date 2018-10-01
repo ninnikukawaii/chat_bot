1e66c75fc0f9ee0b618771c4af3080526f4f5844
TheDoctorTimeLord <38783627+TheDoctorTimeLord@users.noreply.github.com>
2018-10-01 09:39:11 +0500

    пробрасвания исключения наверх

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,10 @@ import logic.ConsoleOutput;
 import logic.MainLoop;
 import logic.ScannerInput;
 
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         MainLoop mainLoop = new MainLoop();
         mainLoop.startLoop(new ScannerInput(), new ConsoleOutput());
     }
