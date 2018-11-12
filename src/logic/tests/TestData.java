c39a0b607c5d0bccf4592b23fe7efbcd8aad3b63
TheDoctorTimeLord <38783627+TheDoctorTimeLord@users.noreply.github.com>
2018-11-12 15:10:32 +0500

    Изменение выдавания вопросов в TestData

--- src/logic/tests/TestData.java
+++ src/logic/tests/TestData.java
@@ -4,26 +4,29 @@ import logic.Question;
 import logic.interfaces.QuestionsData;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
 
 public class TestData implements QuestionsData {
 
-    private ArrayList<Question> testQuestions;
-    private Random random;
+    private List<Question> testQuestions;
+    private Iterator<Question> questionsIterator;
 
     public TestData(){
         testQuestions = new ArrayList<>();
         testQuestions.add(new Question("Как называется пятнистая лошадь?", "Пинто"));
         testQuestions.add(new Question("В какой стране нет комаров?", "Исландия"));
 
-        random = new Random();
+        questionsIterator = testQuestions.iterator();
     }
 
     @Override
     public Question getQuestion() {
-        int index = random.nextInt(testQuestions.size());
-        return testQuestions.get(index);
+        if (!questionsIterator.hasNext()) {
+            questionsIterator = testQuestions.iterator();
+        }
+        return questionsIterator.next();
     }
 
     @Override
